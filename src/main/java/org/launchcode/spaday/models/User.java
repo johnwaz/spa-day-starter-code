8d38467e7e5ce38d5d2154aa5fed61735bb8d6f9
John Wasielewicz <wazjohn@yahoo.com>
2020-10-06 18:08:45 -0500

    Validating form submission data.

--- src/main/java/org/launchcode/spaday/models/User.java
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -1,8 +1,20 @@
 package org.launchcode.spaday.models;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 public class User {
+
+    @NotBlank(message = "Username required")
+    @Size(min = 5, max = 15)
     private String username;
+
+    @Email(message = "Valid email please")
     private String email;
+
+    @NotBlank(message = "Password required")
+    @Size(min = 6)
     private String password;
 
     public User() {
